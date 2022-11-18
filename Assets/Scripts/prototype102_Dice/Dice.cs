ad4e5bf4c3c3fd6cac62865bf90f2ec4b4bbb202
Maja K <115983133+vollkornbiene@users.noreply.github.com>
2022-11-18 11:02:50 +0100

    fix dice roll loose condition

--- Assets/Scripts/prototype102_Dice/Dice.cs
+++ Assets/Scripts/prototype102_Dice/Dice.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
-    public int randomNumber;
     public float step = 1f;
-    //public int luckyNumberA = 1;
-    //public int luckyNumberB = 3;
-    //public int luckyNumberC = 6;
 
-    // array
+    public int randomNumber;
+
+    // initialize array
     public int[] luckyNumbers = { 1, 5, 10, 15, 25, 35, 45, 50 };
     //public int[] luckyNumbers = new int[3];
 
+    // initialize audio
     private AudioSource _AudioSource;
     public AudioClip VictorySound;
+
+    //initialize win bool
+    public bool LuckyNumberWasDrawn;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //luckyNumbers[0] = 1;
-        //luckyNumbers[1] = 3;
-        //luckyNumbers[2] = 6;
-
         _AudioSource = GetComponent<AudioSource>();
     }
 
@@ -38,8 +37,11 @@ public class Dice : MonoBehaviour
        if (Input.GetKeyUp("space"))
         {
            transform.Translate(0, -step, 0);
+
            randomNumber = Random.Range(1, 51);
            print("Du hast eine " + randomNumber + " gewürfelt");
+
+            LuckyNumberWasDrawn = false;
 
 
             for(int i = 0; i < luckyNumbers.Length; i++)
@@ -49,48 +51,16 @@ public class Dice : MonoBehaviour
                 if (randomNumber == luckyNumbers[i])
                 {
                     print("Herzlichen Glückwunsch! " + randomNumber + " ist eine Gewinnzahl!");
+                    LuckyNumberWasDrawn = true;
                     _AudioSource.PlayOneShot(VictorySound);
                 }
 
-                else
+                else if (i == (luckyNumbers.Length-1) && LuckyNumberWasDrawn == false)
                 {
                     print("Niete.");
                 }
             }
 
-
-            //if (randomNumber == luckyNumberA || randomNumber == luckyNumberB || randomNumber == luckyNumberC)
-            //{
-            //    print("Herzlichen Glückwunsch! " + randomNumber + " ist eine Gewinnzahl!");
-            //}
-
-
-            //if (randomNumber == luckyNumbers[0] || randomNumber == luckyNumbers[1] || randomNumber == luckyNumbers[2])
-            //{
-            //    print("Herzlichen Glückwunsch! " + randomNumber + " ist eine Gewinnzahl!");
-            //}
-
-
-            //if(randomNumber == luckyNumberA)
-            //{
-            //    print("Herzlichen Glückwunsch! " + randomNumber + " ist eine Gewinnzahl!");
-            //}
-
-            //else if (randomNumber == luckyNumberB)
-            //{
-            //    print("Herzlichen Glückwunsch! " + randomNumber + " ist eine Gewinnzahl!");
-            //}
-
-            //else if (randomNumber == luckyNumberC)
-            //{
-            //    print("Herzlichen Glückwunsch! " + randomNumber + " ist eine Gewinnzahl!");
-            //}
-
-
-            //else
-            //{
-            //    print("Niete.");
-            //}
         }
     }
 }
