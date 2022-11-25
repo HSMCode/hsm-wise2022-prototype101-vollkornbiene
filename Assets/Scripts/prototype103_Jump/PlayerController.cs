a24142997798ed97d0676ed8c22ae661540f5f7d
Maja K <115983133+vollkornbiene@users.noreply.github.com>
2022-11-25 14:36:46 +0100

    add walking and jumping, animations

--- Assets/Scripts/prototype103_Jump/PlayerController.cs
+++ Assets/Scripts/prototype103_Jump/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float forwardInput;
+    public float speed;
+    public float turnSpeed;
+    public Vector3 force;
+
+    private Animator _playerAnim;
+    private Rigidbody _playerRb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerAnim = GetComponent<Animator>();
+        _playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+
+        _playerAnim.SetFloat("Run", forwardInput);
+
+        if(forwardInput != 0 || horizontalInput != 0)
+        {
+            _playerAnim.SetBool("Walk", true);
+        }
+
+        else
+        {
+            _playerAnim.SetBool("Walk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRb.AddForce(force, ForceMode.Impulse);
+            _playerAnim.SetTrigger("Jump");
+        }
+
     }
 }
